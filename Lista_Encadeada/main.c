b72b8430f16c802d5ea84b2b5878f3d2240f8b36
Victor Hugo Rodrigues da Silva <victorq32@gmail.com>
2016-12-26 00:30:17 -0200

    Lista
    Lista

--- Lista_Encadeada/main.c
+++ Lista_Encadeada/main.c
@@ -5,7 +5,7 @@
 int main()
 {
 
-
+//http://www.cprogressivo.net/2013/10/Lista-simplesmente-encadeada-com-cabeca-em-C-Inserindo-nos-no-inicio-e-fim.html
     printf("Hello world!\n");
     return 0;
 }
